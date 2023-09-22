e20b17949685e54a553907170b3f1685fe3e5106
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-09-22 14:52:59 +0800

    feature: add copy

--- frontend/src/views/Content.vue
+++ frontend/src/views/Content.vue
@@ -5,18 +5,33 @@ import { ref } from "vue";
 import { useMessage } from 'naive-ui'
 import { useGlobalState } from '../store'
 import { api } from '../api'
+import useClipboard from 'vue-clipboard3'
 
+const { toClipboard } = useClipboard()
+const { result } = useGlobalState()
 const message = useMessage()
 const prompt_type = ref('lipstick')
 const prompt = ref('')
 
 const generate = async () => {
-  const { data } = await api.generate(
+  await api.generate(
     prompt_type.value,
     prompt.value || '苹果手机怎么这么贵',
     message
   )
-  result.value = data
+}
+
+const copy = async () => {
+  if (!result.value) {
+    message.error('No result');
+    return
+  }
+  try {
+    await toClipboard(result.value)
+    message.success('Copied');
+  } catch (e) {
+    message.error(e.message || "error");
+  }
 }
 </script>
 
@@ -33,6 +48,9 @@ const generate = async () => {
           placeholder="苹果手机怎么这么贵" />
       </div>
       <div class="block">
+        <n-button v-if="result" @click="copy" tertiary round>
+          复制结果
+        </n-button>
         <n-button @click="generate" tertiary round type="primary">
           生成
         </n-button>
